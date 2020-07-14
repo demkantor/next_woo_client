d88f7d1babfab876b944718bd67708a4a6a845bc
demkantor <demkantor@gmail.com>
2020-07-13 23:30:50 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const Index = () => {
+    return (
+        <div>
+            <Layout>
+                
+            </Layout>   
+        </div>
+    );
+};
+
+export default Index;
